5403eb8070778fdb282e9aae80f03625306c6064
duchaochen <939705214@qq.com>
2018-10-29 17:14:04 +0800

    eureka搭建成功1

---
+++ springcloud-mybatis-springboot-eureka/src/main/java/com/adu/springcloud/SpringcloudMybatisSpringbootEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.adu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer    // EurekaServer服务器端启动类,接受其它微服务注册进来
+public class SpringcloudMybatisSpringbootEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudMybatisSpringbootEurekaApplication.class, args);
+	}
+}
